e2d0651588b760e67a9270c0d847955c8416735b
RBZAHEER <zmulani95@gmail.com>
2025-05-01 11:29:03 +0530

    learn about moudules in terraform and created vpc using modules

--- output.tf
+++ output.tf
@@ -1,25 +1,25 @@
-# Output for count arguments
+# # Output for count arguments
+
+# # output "ec2_public_ip" {
+# #   value = aws_instance.my_instance[*].public_ip
+# # }
+
+# # output "ec2_public_dns" {
+# #   value = aws_instance.my_instance[*].public_dns
+# # }
+
+# # output "ec2_private_ip" {
+# #   value = aws_instance.my_instance[*].private_ip
+# # }
+
+# # output "ec2_az" {
+# #   value = aws_instance.my_instance[*].availability_zone
+# # }
+
+# #Output for forEach arguments
 
 # output "ec2_public_ip" {
-#   value = aws_instance.my_instance[*].public_ip
+#   value = [
+#     for key in aws_instance.my_instance : key.public_ip
+#   ]
 # }
-
-# output "ec2_public_dns" {
-#   value = aws_instance.my_instance[*].public_dns
-# }
-
-# output "ec2_private_ip" {
-#   value = aws_instance.my_instance[*].private_ip
-# }
-
-# output "ec2_az" {
-#   value = aws_instance.my_instance[*].availability_zone
-# }
-
-#Output for forEach arguments
-
-output "ec2_public_ip" {
-  value = [
-    for key in aws_instance.my_instance : key.public_ip
-  ]
-}
